539e2154f5324e55734de629bd7f80897f37dbb9
s-sukriti <eng21ct0039@dsu.edu.in>
2023-11-27 10:25:22 +0000

    Update from https://github.com/Khaanvani-Org/prod/commit/3db0cf5d659a14f451b1dac9b9923d8c9d5cce7c

---
+++ try.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 45
+num2 = 89
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of the numbers {0} and {1} {2}'.format(num1, num2, sum))
